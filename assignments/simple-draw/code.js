ba4aba0fc002503993721f707094e99029029d8f
ion <111394893+ionisea@users.noreply.github.com>
2022-08-19 15:05:21 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,49 +1,4 @@
-/*
- * This code is running in an environment with five functions defined:
- *
- *  drawLine(x1, y1, x2, y2, color, lineWidth)
- *
- *    Draws a line from x1,y1 to x2,y2 using the give color. The fifth argument,
- *    lineWidth, is optional and defaults to 1.
- *
- *  drawCircle(x, y, r, color, lineWidth=1)
- *
- *    Draws a circle centered at x,y with radius r using the given color. The
- *    fith argument, lineWidth, is optional and defaults to 1.
- *
- *  drawRect(x, y, width, height, color, lineWidth=1)
- *
- *    Draws a rectangle starting at x,y with the given width, height, and color.
- *    Positive widths go to the right and negative to the left; positive heights
- *    go down and negative heights go up. The sixth argument, lineWidth, is
- *    optional and defaults to 1.
- *
- *  drawFilledRect(x, y, width, height, color)
- *
- *    Draws a filled rectangle starting at x,y with the given width, height, and
- *    color. Positive widths go to the right and negative to the left; positive
- *    heights go down and negative heights go up.
- *
- *  drawFilledCircle(x, y, r, color)
- *
- *    Draws a filled circle centered at x,y with radius r using the given color.
- *
- * There are also a couple variables that might be useful:
- *
- *  width - the width of the drawing area.
- *  height - the height of the drawing area.
- *
- */
-
-//drawLine(0, 0, width, height, 'teal')
-
-
-
-//drawRect(50, 50, 60, 40, 'black');
-
-//drawFilledCircle(width /2, width -100, 50, 'red');
 var shithead = 1000
-//drawFilledRect(5, height - 200, 45, 15, 'purple');
 while (shithead >0) {
     drawCircle(width/2, height-shithead, 250, '#FFBF00');
    // change the -x number to change the distance between circles, KEEP NEGATIVE!!!!
