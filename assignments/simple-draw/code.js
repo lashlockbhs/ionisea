4dfb44fb227f1f9c7b7677e0068bba6e99a169ee
ion <111394893+ionisea@users.noreply.github.com>
2022-08-24 15:19:03 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -10,4 +10,4 @@ drawFilledRect (width/2 - 100, height/2 - 100, 200, 200, 'white');
 drawFilledRect (width/2 + 100, height/2 - 100, 50, 130, 'white');
 drawFilledRect (width/2 - 100, height/2 + 100, 60, 80, 'white');
 drawFilledRect (width/2 + 40, height/2 + 100, 60, 80, 'white');
-drawFilledRect (width/2 - 90, height/2 - 100, 100, 60, 'white');
+drawFilledRect (width/2 - 90, height/2 - 100, 100, 60, 'black');
