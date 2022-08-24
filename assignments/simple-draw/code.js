bf00d54183a4a6256685dcf578ec39346d3bea2f
ion <111394893+ionisea@users.noreply.github.com>
2022-08-24 15:25:48 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,7 +1,7 @@
 drawFilledRect (0, 0, width, height, 'white');
 var shithead = 1000
 while (shithead >-250000) {
-    drawLine(0, shithead, width, height, 'black');
+    drawLine(0, 0, width, height, 'black');
    // change the -x number to change the distance between circles, KEEP NEGATIVE!!!!
     var shithead = shithead -10 //<- here!
 }
