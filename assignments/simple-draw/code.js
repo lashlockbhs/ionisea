f17776ba3cb2fa9d0ce997f72bb51bd27f815b69
ion <111394893+ionisea@users.noreply.github.com>
2022-08-25 14:42:02 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -10,8 +10,8 @@ while (shithead >-250000) {
 // un mong us code dont touch loser vvvvv
 
 //drawFilledCircle (width/2,height/3, 100, "white")
-//drawFilledRect (width/2 - 100, height/2 - 100, 200, 200, 'white');
-//drawFilledRect (width/2 + 100, height/2 - 100, 50, 130, 'white');
-//drawFilledRect (width/2 - 100, height/2 + 100, 60, 80, 'white');
-//drawFilledRect (width/2 + 40, height/2 + 100, 60, 80, 'white');
-//drawFilledRect (width/2 - 90, height/2 - 100, 100, 60, 'black');
+drawFilledRect (width/2 - 100, height/2 - 100, 200, 200, 'white');
+drawFilledRect (width/2 + 100, height/2 - 100, 50, 130, 'white');
+drawFilledRect (width/2 - 100, height/2 + 100, 60, 80, 'white');
+drawFilledRect (width/2 + 40, height/2 + 100, 60, 80, 'white');
+drawFilledRect (width/2 - 90, height/2 - 100, 100, 60, 'black');
