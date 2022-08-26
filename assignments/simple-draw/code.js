c586b7c001a36ffcf218c2879a4f20061c6f7b92
ion <111394893+ionisea@users.noreply.github.com>
2022-08-26 15:27:34 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -6,20 +6,19 @@ while (shithead >-200000) {
    // change the -x number to change the distance between circles, turn positive and funny little page crash
     var shithead = shithead -4 //<- here!
 }
-//var fujkYou = 0
-//while (fujkYou <100000){
-  //  drawLine(0, fujkYou, width, height, 'maroon'); //you can do it here as well
-   // change the -x number to change the distance between circles, turn positive and funny little page crash
-  //  var fujkYou = fujkYou +10 //<- here!
+var fujkYou = 0
+while (fujkYou <100000){
+  drawLine(0, fujkYou, width, height, 'maroon'); //you can do it here as well
+    //change the -x number to change the distance between circles, turn positive and funny little page crash
+    var fujkYou = fujkYou +10 //<- here!
 //}
 //drawFilledCircle (width/2, height/2, 150)
 
 // un mong us code dont touch vvvvv
 
-drawFilledCircle (width/2,height/3, 100, "blue")
-drawFilledRect (width/2 - 100, height/2 - 100, 200, 200, 'blue');
+//drawFilledCircle (width/2,height/3, 100, "blue")
+//drawFilledRect (width/2 - 100, height/2 - 100, 200, 200, 'blue');
 //drawFilledRect (width/2 + 100, height/2 - 100, 50, 130, 'white');
-drawFilledRect (width/2 - 100, height/2 + 100, 60, 80, 'blue');
-drawFilledRect (width/2 + 40, height/2 + 100, 60, 80, 'blue');
-drawFilledRect (width/2 -75, height/2 - 120, 150, 60, 'teal');
-fillText ('sssss', width/2, height/2)
+//drawFilledRect (width/2 - 100, height/2 + 100, 60, 80, 'blue');
+//drawFilledRect (width/2 + 40, height/2 + 100, 60, 80, 'blue');
+//drawFilledRect (width/2 -75, height/2 - 120, 150, 60, 'teal');
