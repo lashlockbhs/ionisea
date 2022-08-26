b3163a3ce9d2b93934e5d0fd7a8aaf50f52061ba
ion <111394893+ionisea@users.noreply.github.com>
2022-08-26 15:18:23 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -3,14 +3,14 @@ var shithead = 1000
 while (shithead >-200000) {
     drawLine(0, shithead, width, height, 'maroon'); //you can do it here as well
    // change the -x number to change the distance between circles, turn positive and funny little page crash
-    var shithead = shithead -10 //<- here!
+    var shithead = shithead -4 //<- here!
 }
-var fujkYou = 0
-while (fujkYou <100000){
-    drawLine(0, fujkYou, width, height, 'maroon'); //you can do it here as well
+//var fujkYou = 0
+//while (fujkYou <100000){
+  //  drawLine(0, fujkYou, width, height, 'maroon'); //you can do it here as well
    // change the -x number to change the distance between circles, turn positive and funny little page crash
-    var fujkYou = fujkYou +10 //<- here!
-}
+  //  var fujkYou = fujkYou +10 //<- here!
+//}
 //drawFilledCircle (width/2, height/2, 150)
 
 // un mong us code dont touch vvvvv
