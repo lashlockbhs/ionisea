7c481cb735ac1aed3bb378f11e256ade9ffb87d0
ion <111394893+ionisea@users.noreply.github.com>
2022-09-08 15:13:56 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -45,6 +45,5 @@ function isRed (b1,b2,b3,b4){
     //b2='Bloggy'=true;
     //b3='Flibble'=false;
     //b4='Globby'=true;
-    //return b1 || b2 || !b3 || !b4
-    return !flobby || !globby
+    return b1 && b2 && !b3 && !b4
 }
