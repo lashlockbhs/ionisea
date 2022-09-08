11aa01735bb72280fda4936657f4c47629cdf816
ion <111394893+ionisea@users.noreply.github.com>
2022-09-08 14:59:59 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -41,10 +41,10 @@ function eatsMice(red,spotted){
   return !red || !spotted
 }
 function isRed (b1,b2,b3,b4){
-        b1='Flobby';
+    b1='Flobby';
     b2='Bloggy';
     b3='Flibble';
     b4='Globby';
-    return true, true, false, false
+    return b1 && b2
     
 }
