5dfc145b8072eb10795787d9a077966f7ff0f865
ion <111394893+ionisea@users.noreply.github.com>
2022-09-12 15:24:31 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -6,3 +6,5 @@ const advancedPigLatin =(string)=>{
   var advlatin = string.search(/[aeiou]/)
   return advlatin === 0? string.substring(0,string.length)+'way':string.substring(advlatin, string.length)+string.substring(0, advlatin)+'ay'
 }
+var weiu = 3
+var weiu = 62
