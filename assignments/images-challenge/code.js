60140c848e4db4651ca93e4387d2ac7cc5a2d86c
ion <111394893+ionisea@users.noreply.github.com>
2022-09-13 15:12:11 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -31,7 +31,6 @@ while (dist < width){
 const center = width-dist
 var dist=center
 while (dist < width){ 
-var dist = dist+bigness*2
 drawFilledCircle(dist-center/2, height/2, bigness, 'red')
 var dist = dist+bigness*2
 }
