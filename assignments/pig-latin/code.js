f1e8ea1be4cdb3e6e9e2453ef23ddd33a6a95c43
ion <111394893+ionisea@users.noreply.github.com>
2022-09-12 20:22:54 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -1,6 +1,6 @@
 const pigLatin =(string)=>{
   const latin = string.search(/[aeiou]/)
-  return string.substring(latin,string.length)+string.substring(0,latin)+'ay'
+  return string.substring(latin)+string.substring(0,latin)+'ay'
 }
 const advancedPigLatin =(string)=>{ 
   var vowelLoc = string.search(/[aeiou]/)
