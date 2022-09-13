0cbf00851e8e101a6fcf874a09ecb8889204eb85
ion <111394893+ionisea@users.noreply.github.com>
2022-09-13 15:13:58 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -23,7 +23,7 @@
  *
  *  clear()
  */
-const bigness = 28 //just leave it at 5 or 10 please
+const bigness = 120 //just leave it at 5 or 10 please
 var dist = bigness*2
 while (dist < width+10){
   var dist = dist+bigness*2
