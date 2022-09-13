b4ee098df5391e59acbece368c6c8d3a946fa0a4
ion <111394893+ionisea@users.noreply.github.com>
2022-09-13 15:09:44 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -25,7 +25,7 @@
  */
 const bigness = 100 //just leave it at 5 or 10 please
 var dist = bigness
-while (dist < width-bigness){
+while (dist < width){
   var dist = dist+bigness*2
 }
 const center = width-dist
