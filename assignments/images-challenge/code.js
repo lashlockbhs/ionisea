fec404a457da6fcfb3e9e13cd0cbc6f740c55fca
ion <111394893+ionisea@users.noreply.github.com>
2022-09-15 15:17:35 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -78,7 +78,7 @@ let y = 0
 while (y < height-heightextra){ 
   let x = 0
 while (x < width-widthextra){ 
-  drawCircle(x+radius+widthextra/2, y+radius+heightextra/2, radius, 'red')
+  drawCircle(x+radius+widthextra/2, y+radius+heightextra/2, radius, 'blue')
   x += diameter 
   }
   y += diameter
