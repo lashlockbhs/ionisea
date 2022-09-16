b9b765a12396308dc85d45a711a55221854c44f1
ion <111394893+ionisea@users.noreply.github.com>
2022-09-16 15:04:04 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -84,7 +84,7 @@ while (x < width-widthextra){
   y += diameter
   }*/
 //circle code
-drawFilledRect(0, height, width, height, 'blue')
+drawFilledRect(0, 0, width, height, 'blue')
 const howManyCircles = 22
 let radius = width/2/howManyCircles
 let clour = 'red'
