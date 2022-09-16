aeb510dc1add6ab28d70bf088a8973013df761a9
ion <111394893+ionisea@users.noreply.github.com>
2022-09-16 15:11:46 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -84,7 +84,7 @@ while (x < width-widthextra){
   y += diameter
   }*/
 //circle code
-const howManyCircles = 22
+const howManyCircles = 65
 let radius = width/4/howManyCircles
 let clour = 'red'
 while (radius < width/2){
