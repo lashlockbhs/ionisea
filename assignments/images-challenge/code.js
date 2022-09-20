53be8c90eebe6d8d799cdcc14e5d7e44c788d26f
ion <111394893+ionisea@users.noreply.github.com>
2022-09-20 14:54:39 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -97,8 +97,8 @@ while (radius < width/2){
  radius += width/2/howManyCircles
 }*/
 
-
-const pby = 1 //1 is 100% chance, 0.5 is 50, 0.25 is 25, etc
+// again with your code, fillbox with random chance
+const pby = 21 //1 is 100% chance, 0.5 is 50, 0.25 is 25, etc
 const radius = 5
 const diameter = 2*radius
 const widthdist = Math.floor(width/diameter)*diameter
