7771e164d6713f5f310e5cae6744ebcf4a28ef71
ion <111394893+ionisea@users.noreply.github.com>
2022-09-20 16:56:41 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -121,8 +121,8 @@ while (x < width-widthextra){
 }*/
 // grid
 const lineDistance = 15
-let lineY = lineDistance
-let lineX = lineDistance
+let lineY = lineDistance/2
+let lineX = lineDistance/2
 while (lineX < width+lineDistance){
   drawLine(lineX, 0 , lineX, height, 'black')
   lineX += lineDistance
