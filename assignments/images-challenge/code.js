5e0adf8e1825b33459a43e985760debae2e9d875
ion <111394893+ionisea@users.noreply.github.com>
2022-09-21 14:36:44 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -120,7 +120,7 @@ while (x < width-widthextra){
   y += diameter
 }*/
 // grid
-const lineDistance = 15
+const lineDistance = 18
 let lineY = lineDistance/2
 let lineX = lineDistance/2
 while (lineX < width){
