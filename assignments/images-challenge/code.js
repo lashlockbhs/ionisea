ace40f6aa96da95df19d5e6bccf3c94818a0e243
ion <111394893+ionisea@users.noreply.github.com>
2022-09-22 15:26:27 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -156,4 +156,4 @@ while (x < width){
   }
   y += squareSize
 }}
-checkerboard(100); //just keep it kinda reasonable and keep it nice (8 through 70, no numbers that upset me)
+checkerboard(200); //just keep it kinda reasonable and keep it nice (no numbers that upset me)
