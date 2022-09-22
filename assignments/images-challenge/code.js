5e036a89b2061adee86ee3a96fdded49ae264efd
ion <111394893+ionisea@users.noreply.github.com>
2022-09-22 15:24:45 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -156,4 +156,4 @@ while (x < width){
   }
   y += squareSize
 }}
-checkerboard(2500); //just keep it kinda reasonable (8 through 70)
+checkerboard(24); //just keep it kinda reasonable (8 through 70)
