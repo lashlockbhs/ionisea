8e1986e4667f8afe030613e185da76ec215a5579
ion <111394893+ionisea@users.noreply.github.com>
2022-09-28 15:28:59 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -163,7 +163,7 @@ const squareOfCircles = (radius) =>{
     drawCircle(xPos, height-cExtra, radius, 'blue' )
 }
 for (let yPos = cExtra/2; yPos < height - cExtra/2;yPos += diameter){
-  drawCircle((width - height)/2-cExtra, yPos, radius, 'blue')
+  drawCircle((width - height)/2-cExtra/2, yPos, radius, 'blue')
   drawCircle(width - ((width - height)/2+diameter-cExtra), yPos, radius, 'blue')
 }
 }
