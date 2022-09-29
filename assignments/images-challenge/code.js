b7cb520f7cbfb7a0304fc95106b3767645f3d3a3
ion <111394893+ionisea@users.noreply.github.com>
2022-09-29 14:48:11 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -86,7 +86,7 @@ while (x < width-widthextra){
 //conc circle code
 const concCircles = (howManyCircles) =>{
 drawFilledCircle(width/2, height/2, height/2, 'red')
-for (let radius = height/4/howManyCircles; radius < height/2; radius += radius + height/2/howManyCircles){
+for (let radius = height/4/howManyCircles; radius < height/2; radius += + height/2/howManyCircles){
  drawCircle (width/2, height/2, radius, 'blue', height/2/howManyCircles)
  }
 }
