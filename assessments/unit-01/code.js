1526c1c76b10218dca2c1921818c6dcc4bcdd2a1
ion <111394893+ionisea@users.noreply.github.com>
2022-09-30 14:52:18 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -9,3 +9,38 @@ const averageWeight=(weight, num)=>{
   return weight/num
 }
 averageWeight(40,8);
+
+const hypotenuse = (a,b) =>{
+  return Math.sqrt(a^2+b^2)
+}
+hypotenuse(a,b);
+
+const maxRadius=(width,height)=>{
+  return 1/2(Math.min(width,height))
+}
+maxRadius(width,height);
+
+const numCircles=(radius,width)=>{
+  return Math.floor(width/(radius*2))
+}
+numCircles(radius, width);
+
+const offset = (cWidth, fWidth) =>{
+  return 1/2(cWidth-fWidth)
+}
+offset(cWidth,fWidth);
+
+const canSleepIn = (weekday, vacation) =>{
+  if (!weekday || vacation){
+    return true
+  } else {
+    return false
+  }
+}
+canSleepIn(weekday,vacation);
+
+const canGoToProm =(senior, invitedBySenior, onExcList) =>{
+  if(senior || invitedBySenior && !onExcList){
+    
+  }
+}
