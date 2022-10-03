45df929486bedc24a439b5ef154956d4476bbbf3
ion <111394893+ionisea@users.noreply.github.com>
2022-10-03 14:58:06 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -152,7 +152,7 @@ for(let x = 0; x < height; x+=squareSize){
 const squareOfCircles = (radius, color) =>{
   const diameter = 2*radius
   const cNum = Math.floor(height/diameter)
-  const cExtra = (height - (diameter*cNum))/2+diameter
+  const cExtra = (height - (diameter*cNum))/2+radius
   let xCheese;
   for(let xPos = (width - height)/2+cExtra;xPos < width - ((width - height))/2-cExtra; xPos += diameter){
     drawCircle(xPos, height - cExtra, radius, color)
