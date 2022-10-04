fea26c6f356066b887d85fd0632e47a82b2dbfad
ion <111394893+ionisea@users.noreply.github.com>
2022-10-04 15:24:51 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -126,12 +126,12 @@ while (lineY < height){
 
 const checkerboard = (numSq, color1, color2) =>{
 const shortSide = Math.min(height,width)
-const longSide = Math.max(height, width)
+//const longSide = Math.max(height, width)
 const squareSL = shortSide/numSq // side length/size
 drawFilledRect((width-height)/2, 0, shortSide, shortSide, color2)
 for (let yPos = 0; yPos < height; yPos += squareSL){
   for (let xPos = (width-height)/2; xPos < width - width-height/2; xPos += 2*squareSL){
-    drawFilledRect(xPos, 20, squareSL, squareSL, color1)
+    drawFilledRect(xPos, yPos, squareSL, squareSL, color1)
   }
 }
 }
