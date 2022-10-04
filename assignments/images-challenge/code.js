788b6177ea64590aa3097aa2161db9af6736c1e7
ion <111394893+ionisea@users.noreply.github.com>
2022-10-04 15:31:14 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -131,7 +131,7 @@ let offset = 0;
 const squareSL = shortSide/numSq // side length/size
 drawFilledRect((width-height)/2, 0, shortSide, shortSide, color2)
 for (let yPos = 0; yPos < height; yPos += squareSL){
-  for (let xPos = (width-height)/2 + offset * squareSL; xPos < width - (width-height)/2 - 1; xPos += 2*squareSL){
+  for (let xPos = (width-height)/2 + offset * squareSL; xPos < width - (width-height)/2 - squareSL/2; xPos += 2*squareSL){
     drawFilledRect(xPos, yPos, squareSL, squareSL, color1)
   }
   if (offset == 0){
