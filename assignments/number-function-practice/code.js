e2a5771f9997c6741301e1cb0619d9bb28757fa6
ion <111394893+ionisea@users.noreply.github.com>
2022-10-06 15:12:25 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -1,4 +1,8 @@
-const array1 = [2, 5, 6];
-array1.pop()
-array1.push('\\]\]]\]\]')
+const array1 = [
+  [2, 5, 6],
+  [7, 2, 9],
+  [3, 4, 8],
+  ];
+array1[2].pop()
+array1[2].push(array1)
 console.log(array1)
