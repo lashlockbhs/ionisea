51d0956c1b8889652f0f724a305912937103f07f
ion <111394893+ionisea@users.noreply.github.com>
2022-10-06 15:00:42 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -5,82 +5,82 @@ const emit = (a, b, product) => {
 };
 
 // Write your code here ...
-const averageWeight=(weight, num)=>{
-  return weight/num
+const averageWeight = (weight, num) => {
+  return weight / num
 }
-averageWeight(40,8);
+averageWeight(40, 8);
 
-const hypotenuse = (a,b) =>{
-  return Math.sqrt(a**2+b**2)
+const hypotenuse = (a, b) => {
+  return Math.sqrt(a ** 2 + b ** 2)
 }
 hypotenuse();
 
-const maxRadius=(width,height)=>{
-  return (Math.min(width,height))/2
+const maxRadius = (width, height) => {
+  return (Math.min(width, height)) / 2
 }
 maxRadius();
 
-const numCircles=(radius,width)=>{
-  return Math.floor(width/(radius*2))
+const numCircles = (radius, width) => {
+  return Math.floor(width / (radius * 2))
 }
 numCircles(20, 500);
 
-const offset = (cWidth, fWidth) =>{
-  return (cWidth-fWidth)/2
+const offset = (cWidth, fWidth) => {
+  return (cWidth - fWidth) / 2
 }
 
-const canSleepIn = (weekday, vacation) =>{
+const canSleepIn = (weekday, vacation) => {
   return (!weekday || vacation)
 }
-canSleepIn(false,false);
+canSleepIn(false, false);
 
-const canGoToProm =(senior, invitedBySenior, onExcList) =>{
+const canGoToProm = (senior, invitedBySenior, onExcList) => {
   return ((senior || invitedBySenior) && !onExcList)
 }
 canGoToProm(true, false, true);
 
-const getsSpeedingTicket=(speed, grouchyCop) =>{
-  return (((speed > 65)&& grouchyCop) || speed > 70)
+const getsSpeedingTicket = (speed, grouchyCop) => {
+  return (((speed > 65) && grouchyCop) || speed > 70)
 }
 getsSpeedingTicket(66, true);
 
-const moreThanTwiceAsLong = (string1,string2) => {
+const moreThanTwiceAsLong = (string1, string2) => {
   return (string1.length > string2.length * 2)
 }
 
-const aFartherThanB = (a,b,c) =>{
-return Math.abs(c-a) > Math.abs (c-b);
+const aFartherThanB = (a, b, c) => {
+  return Math.abs(c - a) > Math.abs(c - b);
 }
-aFartherThanB(0,2,15);
+aFartherThanB(0, 2, 15);
 
 const firstHalf = (string) => {
-  return string.substring(0,Math.floor(string.length/2))
+  return string.substring(0, Math.floor(string.length / 2))
 }
 firstHalf('howisyourday');
 
 const secondHalf = (string) => {
-  return string.substring(Math.floor(string.length/2))
+  return string.substring(Math.floor(string.length / 2))
 }
 secondHalf('wowitworkssick');
 
-const upDown = (string) =>{
-  return string.toUpperCase()+ string.toLowerCase()
+const upDown = (string) => {
+  return string.toUpperCase() + string.toLowerCase()
 }
 upDown('insertusfulmessagehere');
 
-const everyOther=(string)=>{
-  return string[0]+string[2]+string[4] //just the 1st, 3rd, and 5th
+const everyOther = (string) => {
+  return string[0] + string[2] + string[4] //just the 1st, 3rd, and 5th
 }
 everyOther('woahcoolitworks');
 
-const upDownLastCharacter = (string) =>{
-  return string[string.length-1].toUpperCase()+string[string.length-1]
+const upDownLastCharacter = (string) => {
+  return string[string.length - 1].toUpperCase() + string[string.length - 1]
 }
 upDownLastCharacter('ohmanihavelotsofideasforthesestrings');
 
-const yesIfEven=(num)=>{
-  if (num/2 == Math.floor(num/2)){
-  return 'yes'
+const yesIfEven = (num) => {
+  if (num / 2 == Math.floor(num / 2)) {
+    return 'yes'
   } else {
     return 'no'
   }
