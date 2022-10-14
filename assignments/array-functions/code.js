e651b47504a6c495b82b270baf972b74bd0aa23a
ion <111394893+ionisea@users.noreply.github.com>
2022-10-14 15:01:49 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,7 +1,7 @@
 const countTens = (array) =>{
   let numOfTens = 0;
   for (let pos = 0; pos < array.length; pos += 1){
-    if (array[pos] = 10){
+    if (array[pos] = 5){
       numOfTens += 1
     }
   }
