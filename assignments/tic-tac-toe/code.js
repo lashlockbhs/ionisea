05f3db1ebaf7cea72ad6bfe2938f11eb9ae1d9a5
ion <111394893+ionisea@users.noreply.github.com>
2022-10-17 15:30:48 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -10,7 +10,7 @@ const max = Math.max(width,height)
 const min = Math.min(width,height)
 let player = 'X';
 for (let editConst = 1; editConst > 0; editConst -= 1/3){
-  drawLine(max/2 + (min/2*editConst), height, max/2 + min/2*editConst, 0, 'black', 2)
+  drawLine(max/2 + (min*editConst), height, max/2 + min*editConst, 0, 'black', 2)
 }
 for (let totalTurns = 0; totalTurns < 9; totalTurns++){
 registerOnclick((x, y) => {
