b4bf7857058ed97c41a4cec4e9fa3305745f002d
ion <111394893+ionisea@users.noreply.github.com>
2022-10-17 15:11:04 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -9,5 +9,5 @@ for (let totalTurns = 0; totalTurns <= 9; totalTurns++){
 registerOnclick((x, y) => {
   drawText(player, x-20, y+20, 'black', Math.min(width, height) * 0.3);
 });
-player == 'x' ? player = 'o' : player;
+player == 'x' ? player = 'o' : player = 'x';
 }
