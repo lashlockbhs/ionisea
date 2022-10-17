4707d86464efff39d60f9ff006cd3c52efa71893
ion <111394893+ionisea@users.noreply.github.com>
2022-10-17 15:26:48 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -4,9 +4,14 @@
 // of the equals sign in our normal `const foo = ...` function definition. This
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
+// for me:  *  drawLine(x1, y1, x2, y2, color, lineWidth)
+
 const max = Math.max(width,height)
 const min = Math.min(width,height)
 let player = 'X';
+for (let editConst = 1; editConst > 0; editConst -= 1/3){
+  drawLine(max/2 - min + (min*editConst), min*2/3, max/2 - min*editConst, min*2/3, 'black', 2)
+}
 for (let totalTurns = 0; totalTurns < 9; totalTurns++){
 registerOnclick((x, y) => {
   drawText(player, x-min*0.1, y+min*0.1, 'black', min * 0.3);
