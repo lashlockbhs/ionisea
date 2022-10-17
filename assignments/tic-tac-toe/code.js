7ac963b1f9b8bd283c3f8fc18580fb09906968ae
ion <111394893+ionisea@users.noreply.github.com>
2022-10-17 15:12:11 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -4,10 +4,10 @@
 // of the equals sign in our normal `const foo = ...` function definition. This
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
-let player = 'x';
+let player = 'X';
 for (let totalTurns = 0; totalTurns <= 9; totalTurns++){
 registerOnclick((x, y) => {
   drawText(player, x-20, y+20, 'black', Math.min(width, height) * 0.3);
-  player == 'x' ? player = 'o' : player = 'x';
+  player == 'X' ? player = 'O' : player = 'X';
 });
 }
