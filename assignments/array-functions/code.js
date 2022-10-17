4f9d0b5ff6772093f2d175315341b4c5e840ef05
ion <111394893+ionisea@users.noreply.github.com>
2022-10-17 15:01:14 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,15 +1,15 @@
 const countTens = (array) =>{
   let numOfTens = 0;
-  for (let pos = 0; pos < array.length; pos += 1){
-    array[pos] == 10 ? numOfTens += 1:numOfTens;
+  for (const element of array){
+    element == 10 ? numOfTens += 1:numOfTens;
   }
   return numOfTens
 }
 
 const sum = (array) => {
   let total = 0;
-  for(let pos = 0; pos < array.length; pos += 1){
-    total += array[pos]
+  for(const element of array){
+    total += element
   }
   return total
 }
