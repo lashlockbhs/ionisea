d1e5eeb5fa3fc952190df2bbe41547cad55cdeb0
ion <111394893+ionisea@users.noreply.github.com>
2022-10-19 15:06:59 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -17,6 +17,9 @@ for (let editConst = 0; editConst <= 1; editConst += 1/3){
   drawLine(max/2-min/2 + min*editConst, height, max/2-min/2 + min*editConst, 0, 'black', 2)
   drawLine(max/2-min/2, height*editConst, max/2+min/2, height*editConst, 'black', 2)
 }
+const shher = (array) =>{
+return array.findIndex((element) => element >12)
+}
 let turns = 0; 
 registerOnclick((x, y) => {
   //for (let totalTurns = 0; totalTurns < 9; totalTurns++){ // (does not work!!!!!!! used if )
