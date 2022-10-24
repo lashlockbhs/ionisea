44f0db1c1f7d6957a5ca7f0dfe28387b56a236fa
ion <111394893+ionisea@users.noreply.github.com>
2022-10-24 15:14:41 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -35,8 +35,8 @@ registerOnclick((x, y) => {
   } else if (y < height) {
     yPos = yPosConst + 2/3 * height
   }
-  if (x < max/2 - min/2){
-    return 'dumbass'}
+  if (x < max/2 - min/2 || x > max/2 + min/2){
+    console.log ('dumbass')}
   if (x < max/2 - min/2 + 1/3*min){
     xPos = xPosConst
   } else if (x < max/2 + min * 1/6){
