32de7b42249cec5e0abe3fc6b9c4819e895f26d8
ion <111394893+ionisea@users.noreply.github.com>
2022-10-25 14:45:05 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -24,13 +24,13 @@ let yPos;
 let player = 'X';
 registerOnclick((x, y) => {
   if (turns <9){
- /*if (y < 1/3 * height){
+ if (y < 1/3 * height){
     yPos = 0
   } else if (y < 2/3 * height){
     yPos = 1
   } else if (y < height) {
     yPos = 2
-  } */
+  } 
   if (x < max/2 - min/2 || x > max/2 + min/2){
    return console.log ('dumbass')
    }
@@ -41,8 +41,9 @@ registerOnclick((x, y) => {
   } else if (x < max/2 + min/2){
     xPos = 2
   } 
-  y < 1/3 * height ? yPos = 0 : y < 2/3 * height ? yPos = 1 : yPos = 2;
+  //y < 1/3 * height ? yPos = 0 : y < 2/3 * height ? yPos = 1 : yPos = 2;
   console.log (coordArray[yPos][xPos])
+  coordArray[yPos][xPos].push(player)
   turns++
   console.log('turn: ' + turns, 'coords: ' ,x,y)
   drawText(player, max/2-min/2 - min*0.1 +min/6 + (min * xPos/3), min*0.1 + min/6 + min*yPos/3, 'black', min * 0.3)
