c54e82013095bdee379a82288074be30e46cd234
ion <111394893+ionisea@users.noreply.github.com>
2022-10-27 14:44:58 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -8,5 +8,5 @@ const firstAndLast = (string) =>{
   return string[0] + string[string.length -1]
 }
 const swapFrontAndBack = (string) => {
-  return string[string.length] + string.substring(1,string.length-1) + string[0]
+  return string[string.length-1] + string.substring(1,string.length-2) + string[0]
 }
