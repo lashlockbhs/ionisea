47fb4dd301bc99e9f5988913907d00dbaaf8b18e
ion <111394893+ionisea@users.noreply.github.com>
2022-10-27 14:58:31 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -26,5 +26,5 @@ const secondHalf = (string) =>{
   return string.substring(Math.floor(string.length/2), string.length)
 }
 const upDown = (string) => {
-  return string[string.length].toUpperCase() + string[string.length].toLowerCase()
+  return string[string.length-1].toUpperCase() + string[string.length-1].toLowerCase()
 }
