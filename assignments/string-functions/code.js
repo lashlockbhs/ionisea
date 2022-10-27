be3cb2789f593ba7517e215d63cb34a77909eef0
ion <111394893+ionisea@users.noreply.github.com>
2022-10-27 14:40:27 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -2,5 +2,5 @@ const findFnord = (string) =>{
   return string.search("fnord")
 }
 const stringContains = (string1, string2) => {
-  return string1.search(string2)
+  return string1.search(string2) != -1
 }
