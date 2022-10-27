c7b0d49ff2b8eb103f21f7bc36256a9f778a150e
ion <111394893+ionisea@users.noreply.github.com>
2022-10-27 14:34:59 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,10 +1,5 @@
-// This is a bit of a new thing. registerOnclick is a function provided by the
-// framework. But the argument we're passing to it is *another* function. Notice
-// how the argument here looks like what we normally put on the righthand side
-// of the equals sign in our normal `const foo = ...` function definition. This
-// is called an anonymous function. We'll discuss this in more detail in a few
-// weeks but for now you can just adapt this code.
 // for me:  *  drawLine(x1, y1, x2, y2, color, lineWidth)
+
 const coordArray = [
   [[], [], []],
   [[], [], []],
