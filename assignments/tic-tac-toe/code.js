61464b04eb8e181e664dd098b74a57aecbf06a49
ion <111394893+ionisea@users.noreply.github.com>
2022-10-31 15:28:55 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -10,8 +10,8 @@ const winSearch = (playerA, playerB) =>{
   let i = playerB || ''
   let winner;
   for(let c = 0; c<2; c++){
-    (coordArray[c][0] == s) ? (coordArray[c][1] == s) ?(coordArray [c][2] == s) ? winner = s :0 :0 :0
-    (coordArray[0][c] == s) ? (coordArray[1][c] == s) ?(coordArray [2][c] == s) ? winner = s :undefined :undefined :undefined
+    (coordArray[c][0] == s) ? (coordArray[c][1] == s) ?(coordArray[c][2] == s) ? winner = s :0 :0 :0
+    (coordArray[0][c] == s) ? (coordArray[1][c] == s) ?(coordArray[2][c] == s) ? winner = s :undefined :undefined :undefined
   }
   return winner
 }
