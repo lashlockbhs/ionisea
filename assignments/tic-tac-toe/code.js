fa148cfec0c7828cffd9a327989181abb18e1e7e
ion <111394893+ionisea@users.noreply.github.com>
2022-11-02 14:36:25 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -7,12 +7,11 @@ const coordArray = [
 ]
 let winner;
 const winSearch = (player) =>{
-  let s = player
   for(let c = 0; c<=2; c++){
-    (coordArray[c][0] == s) ? (coordArray[c][1] == s) ?(coordArray[c][2] == s) ? winner = s :0 :0 :0;
-    (coordArray[0][c] == s) ? (coordArray[1][c] == s) ?(coordArray[2][c] == s) ? winner = s :0 :0 :0;
-    (coordArray[0][0] == s) ? (coordArray[1][1] == s) ?(coordArray[2][2] == s) ? winner = s :0 :0 :0;
-    (coordArray[2][0] == s) ? (coordArray[1][1] == s) ?(coordArray[0][2] == s) ? winner = s :0 :0 :0;
+    (coordArray[c][0] == player) ? (coordArray[c][1] == player) ?(coordArray[c][2] == player) ? winner = player :0 :0 :0;
+    (coordArray[0][c] == player) ? (coordArray[1][c] == player) ?(coordArray[2][c] == player) ? winner = player :0 :0 :0;
+    (coordArray[0][0] == player) ? (coordArray[1][1] == player) ?(coordArray[2][2] == player) ? winner = player :0 :0 :0;
+    (coordArray[2][0] == player) ? (coordArray[1][1] == player) ?(coordArray[0][2] == player) ? winner = player :0 :0 :0;
   }
   return winner
 }
