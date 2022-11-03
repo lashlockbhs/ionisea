e4d51eee6851d1f0ca6b02914f0dcc1ff11566c8
ion <111394893+ionisea@users.noreply.github.com>
2022-11-03 15:02:34 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -47,7 +47,7 @@ registerOnclick((x, y) => {
     xPos = 2
   } 
   //y < 1/3 * height ? yPos = 0 : y < 2/3 * height ? yPos = 1 : yPos = 2;
-  if (coordArray[yPos][xPos] == '' && winner == undefined){
+  if (coordArray[yPos][xPos] == '' && winSearch(player) == undefined){
     coordArray[yPos][xPos].push(player)
     drawText(player, max/2-min/2 - min*0.1 +min/6 + (min * xPos/3), min*0.1 + min/6 + min*yPos/3, 'black', min * 0.3)
     console.log (coordArray, 'most recent: ', xPos, yPos, winSearch(player))
