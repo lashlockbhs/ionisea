8c8fc4666ce13fdd613729a382bf734e6e2c51f5
ion <111394893+ionisea@users.noreply.github.com>
2022-11-03 15:13:56 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -16,6 +16,7 @@ const winSearch = (player) =>{
     (coordArray[2][0] == player) ? (coordArray[1][1] == player) ?(coordArray[0][2] == player) ? winner = player:0 :0 :0;
 
   }
+  console.log(winner.toString())
   return winner
 }
 const max = Math.max(width,height)
@@ -51,7 +52,7 @@ registerOnclick((x, y) => {
     coordArray[yPos][xPos].push(player)
     drawText(player, max/2-min/2 - min*0.1 +min/6 + (min * xPos/3), min*0.1 + min/6 + min*yPos/3, 'black', min * 0.3)
     console.log (coordArray, 'most recent: ', xPos, yPos, winSearch(player))
-    winSearch(player) == player ? drawText(player + ' wins!', width/4, height/2,Math.floor(Math.random()*16777215).toString(), height/2) : 0;
+    winSearch(player) == player ? drawText(player + ' wins!', width/4, height/2,player == player1 ? 'red' : 'blue', height/2) : 0;
     player == player1 ? player = player2 : player = player1;
     turns++
     turns == 9 && winSearch(player) == undefined ? drawText('draw', width/12, height*0.8, 'red', height): 0;
