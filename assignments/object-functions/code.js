1ac38dbba70a4112de61bac4ca6ac6ff2aef6fb7
ion <111394893+ionisea@users.noreply.github.com>
2022-11-07 15:06:35 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -11,5 +11,5 @@ const distance = (p1, p2) =>{
   return Math.sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2)
 }
 const midpoint = (p1, p2) =>{
-  return {x: Math.abs(p1.x - p2.x), y: Math.abs(p1.y - p2.y)}
+  return {x: (Math.abs(p1.x) - Math.abs(p2.x)), y: (Math.abs(p1.y) - Math.abs(p2.y))}
 }
