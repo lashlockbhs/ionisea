aec39f84d703e1c74c0e868c29e48de83bd90705
ion <111394893+ionisea@users.noreply.github.com>
2022-11-09 14:34:49 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -8,7 +8,7 @@ const firstAndLast = (string) =>{
   return string[0] + string[string.length -1]
 }
 const swapFrontAndBack = (string) => {
-  return string.substring(Math.floor(string.length/2), string.length) + string.substring(0,string.length/2)
+  return string.substring((string.length/2), string.length) + string.substring(0,string.length/2)
 }
 const simplePigLatin =(string, vIndex)=>{
   return string.substring(vIndex)+string.substring(0,vIndex)+'ay'
