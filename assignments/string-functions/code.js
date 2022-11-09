c5425b96be1aa3fd2d8ed4ea879c69a5868bd973
ion <111394893+ionisea@users.noreply.github.com>
2022-11-09 14:47:06 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -2,7 +2,7 @@ const findFnord = (string) =>{
   return string.indexOf("fnord")
 }
 const stringContains = (string1, string2) => {
-  return string1.search(string2) != -1
+  return string1.indexOf(string2) != -1
 }
 const firstAndLast = (string) =>{
   return string[0] + string[string.length -1]
