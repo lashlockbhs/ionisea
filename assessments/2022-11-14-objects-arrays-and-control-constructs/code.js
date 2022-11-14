8231ee4ba56932b814956db8a1bda27156f7515d
ion <111394893+ionisea@users.noreply.github.com>
2022-11-14 14:53:25 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -6,9 +6,11 @@ const area = (rect) => {
 };
 
 const higherPaid = (e1, e2) => {
+  return Math.max(e1.salary, e2.salary)
 };
 
 const isSamePoint = (p1, p2) => {
+  return p1 == p2
 };
 
 const totalWithTip = (bill, tipPercentage) => {
