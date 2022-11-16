f9b55f22cceba5362da22f9db0db869d70602d91
ion <111394893+ionisea@users.noreply.github.com>
2022-11-16 15:03:47 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -1,7 +1,7 @@
 registerOnclick((x,y) => {
   let acc = 100000;
   for (let x=0; x< 250000000; x++){
-    const l = Math.round(Math.random()/Math.random())*(x+y)
+    const l = Math.round(Math.random()/Math.random())
     if (acc<l) acc=l;
   }
   let sciNote = acc.toString()[0] + '.' + acc.toString().substring(1,5) + '*' + '10' + '^' + (acc.toString().length - 1)
