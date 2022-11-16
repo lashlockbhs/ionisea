ae597d23a71f43bda97096d366719b237df6541f
ion <111394893+ionisea@users.noreply.github.com>
2022-11-16 14:48:44 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -1,7 +1,7 @@
 registerOnclick((x,y) => {
   let acc = 1;
   for (let x=0; x< 250000000; x++){
-    const l = Math.random()/Math.random()
+    const l = Math.round(Math.random()/Math.random())
     acc<l ? acc=l :0;
   }
   drawText(acc,x,y,'black',25)
