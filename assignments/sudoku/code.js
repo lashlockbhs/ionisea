d2bfe6e216f40348c31c6e20b4c8108b98b12b6f
ion <111394893+ionisea@users.noreply.github.com>
2022-11-16 15:29:06 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -9,5 +9,5 @@ registerOnclick((x,y) => {
   acc = acc.toString()
   let sciNote = acc[0] + '.' + acc.substring(1,6) + ' · ' + '10' + '^' + (acc.length - 1)
   console.log(acc, ' // ', sciNote, '//', sciNote[sciNote.length-1] + '; from', attempts, 'attempts')
-  drawText(mode == 'sci' ? sciNote : mode == 'power' ? sciNote[sciNote.length-1] :mode == 'commas'? acc.substring(0,3) + ',' + acc.substring(3,6) + ',' + acc.substring(6,9) : acc,x,y,'black',25)
+  drawText(mode == 'sci' ? sciNote : mode == 'power' ? sciNote[sciNote.length-1] : acc,x,y,'black',25)
 });
