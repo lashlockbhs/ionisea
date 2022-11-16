31cfc0b75fc3b6ceea69de37388ce956d429c6c0
ion <111394893+ionisea@users.noreply.github.com>
2022-11-16 15:18:15 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -1,11 +1,12 @@
+const attempts = 1000000
+const mode = 'power' //'sci', 'power', (anything else will return literal value)
 registerOnclick((x,y) => {
   let acc = 0;
-  const attempts = 1000000
   for (let x=0; x< attempts; x++){
     const l = Math.round(Math.random()/Math.random())
     if (acc<l) acc=l;
   }
   let sciNote = acc.toString()[0] + '.' + acc.toString().substring(1,6) + ' · ' + '10' + '^' + (acc.toString().length - 1)
   console.log(sciNote, ' // ', acc + '; from', attempts, 'attempts')
-  drawText(sciNote,x,y,'black',25)
+  drawText(mode == 'sci' ? sciNote : mode == 'power' ? sciNote[sciNote.length] : acc,x,y,'black',25)
 });
