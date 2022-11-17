3f86f1ea13d8cde03eb6877fb9cb270b67d20085
ion <111394893+ionisea@users.noreply.github.com>
2022-11-17 15:26:12 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -1,7 +1,13 @@
 const attempts = 1000000 //1 million is best - can go up to maybe 2 billion before crashing (will take about a minute at 1 billion)
-const mode = 'power' //'sci' or 'power'(anything else will return literal value)
+const mode = 'conc' //'sci', 'power', 'conc' (anything else will return literal value)
 const sciNote = (acc) => {
  return (acc[0] + '.' + acc.substring(1,6) + ' · ' + '10' + '^' + (acc.length - 1)).toString()
+}
+const roundConcat = (acc) =>{
+  if (acc.length <4) return acc;
+  else if (acc.length <7) return acc.substring(0,acc.length - 3) + 'k'
+  else if (acc.length <10) return acc.substring(0,acc.length - 6) + 'm'
+  else return acc.substring (0, acc.length - 9) + 'b'
 }
 const checkIfGood = (acc)=>{
   if (acc.length-1 >= (attempts.toString().length)  || (acc.length < (attempts.toString().length-1))) {
@@ -18,5 +24,5 @@ registerOnclick((x,y) => {
   }
   acc = acc.toString()
   console.log(acc, '/ from', attempts, 'attempts')
-  drawText(mode == 'sci' ? sciNote(acc) : mode == 'power' ? acc.length-1 : acc, x, y, checkIfGood(acc),25)
+  drawText(mode == 'sci' ? sciNote(acc) : mode == 'power' ? acc.length-1 : mode == 'conc' ? roundConcat(acc) : acc, x, y, checkIfGood(acc),25)
 });
