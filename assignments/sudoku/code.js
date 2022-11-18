b3d85ecbaeb76f24afccb9e4e12f08aa52d54905
ion <111394893+ionisea@users.noreply.github.com>
2022-11-17 19:32:44 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -27,7 +27,7 @@ start by choosing the amount of 'attempts' every time you click.
 */
 
 
-const attempts = 1 
+const attempts = 100000 
 const multiplier = 1 //this should not exceed a few million or things WILL break
 const mode = 'trunc' //'sci', 'power', 'trunc', 'avg', 'page.Crash()' (this will not do anything)
 let logs = {total: 0, jackpots: 0, highAnomalies: 0, lowAnomalies: 0, anomalies: 0, array: []} //check your results by typing logs.(what you want here) into repl
