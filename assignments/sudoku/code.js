fcc54e8f145103320284f5474c462cfa224f9e27
ion <111394893+ionisea@users.noreply.github.com>
2022-11-18 15:28:26 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -64,7 +64,7 @@ const checkIfAnomaly = (acc)=>{
     logs.highAnomalies++
     logs.anomalies++
     if (acc.length-2 >= (attempts.toString().length)){ 
-      drawText('jackpot with ' + (mode == 'sci' ? sciNote(acc) : mode == 'power' ? acc.length-1 : mode == 'trunc' ? truncate(acc) : mode == 'commas' ? commas(acc): acc) + '!', Math.random()*width - height, Math.random()*height, 'blue', height/6)
+      drawText('jackpot with ' + (mode == 'sci' ? sciNote(acc) : mode == 'power' ? acc.length-1 : mode == 'commas' ? commas(acc): truncate(acc)) + '!', Math.random()*width - height, Math.random()*height, 'blue', height/6)
       logs.jackpots++
       return 'blue'
     }
