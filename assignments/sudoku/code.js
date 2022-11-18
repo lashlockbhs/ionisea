7c75f8d4049b643a040c426872c07e5eb87a739c
ion <111394893+ionisea@users.noreply.github.com>
2022-11-18 15:22:59 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -83,7 +83,7 @@ registerOnclick((x,y) => {
   let acc = 0;
   for (let e=0; e < attempts; e++){
     const l = Math.round(Math.random()/Math.random())
-    if (acc<l* multiplier) acc=l*multiplier);
+    if (acc<l* multiplier) acc=l*multiplier;
   }
   logs.array.push(acc)
   acc = acc.toString()
