10ffc92c86034dbb6fb802bc263905b3f102397d
ion <111394893+ionisea@users.noreply.github.com>
2022-11-23 09:06:11 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -15,7 +15,7 @@ const BLACK_PAWN   = '♟';
 drawText(WHITE_KING, width/2, height/2, 'black', 64);
 const randArrayAvg = (arr) =>{
 for (let x = 0; x<14; x++){
-  arr.push(Math.round(Math.random()*10))
+  arr.push(Math.round(Math.random()*100))
 }
 return {array: arr, arrayValue: arr.reduce((x, c) => x+c,0), arrayAvg: ( arr.reduce((x, c) => x+c,0))/arr.toString().length
 }}
