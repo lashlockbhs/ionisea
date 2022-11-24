2b98a84a0c2fd4bff77be01650c3826cd7dd2736
ion <111394893+ionisea@users.noreply.github.com>
2022-11-24 08:57:27 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -24,7 +24,7 @@ return {array: arr, arrayValue: arr.reduce((x, c) => x+c,0), arrayAvg: ( arr.red
 const jackpotOddsTest = (count, times) =>{ //keep above as 1000, x in the repl 
   const jackpotOdds = []
 for (let test =0; test < times; test++){
-jackpotOdds.push((count - randArrayAvg(count).array.indexOf(100))/(count*0.01))
+jackpotOdds.push(count/(count - randArrayAvg(count).array.indexOf(100))/(count*0.01))
 }
 return jackpotOdds
 }
