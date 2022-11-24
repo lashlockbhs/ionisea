553909ed110e45621844dd5066af6f458567bb5f
ion <111394893+ionisea@users.noreply.github.com>
2022-11-24 08:43:52 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -21,10 +21,10 @@ for (let x = 0; x<count; x++){
 const ordered = arr.sort((a,b) => a-b)
 return {array: arr, arrayValue: arr.reduce((x, c) => x+c,0), arrayAvg: ( arr.reduce((x, c) => x+c,0))/arr.length, arrayMedian: ordered%2 == 0 ? arr[arr.length.ceil/2] : (arr[arr.length/2]+arr[arr.length/2+1])/2}
 }
-const jackpotOddsTest = (count, times) =>{
+const jackpotOddsTest = (count, times) =>{ //leave as 1000, 100 in the repl preferably
   const jackpotOdds = []
 for (let test =0; test < times; test++){
-jackpotOdds.push((count - randArrayAvg(count).array.indexOf(100))/10)
+jackpotOdds.push((count - randArrayAvg(count).array.indexOf(100))/1000)
 }
 return jackpotOdds
 }
