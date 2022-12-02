f315f350e99bc4efe654d2d1b8f93ecc97b0f9a1
ion <111394893+ionisea@users.noreply.github.com>
2022-12-02 20:48:58 +0000

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -32,7 +32,7 @@ This program starts when you click on
 
 const attempts = 1000000
 const multiplier = 1 //this should not exceed a few million or things WILL break (also breaks with negatives)
-const mode = 3 // 0-5, explained in the guide
+const mode = 2 // 0-5, explained in the guide
 let logs = {total: 0, jackpots: 0, highAnomalies: 0, lowAnomalies: 0, anomalies: 0, array: []} 
 
 // Notation functions
@@ -68,7 +68,7 @@ const convertAccMode = (acc) =>{
   } else if (mode == 1) {
     return sciNote(acc)
   } else if (mode == 2){
-    return Math.ceil(Math.log10(acc)).toString()
+    return Math.ceil(Math.log10(acc))
   } else if (mode == 3){
     return truncate(acc)
   } else if (mode == 4){
