719cd00802db73668dc52c9e33929f9a10f5f31f
ion <111394893+ionisea@users.noreply.github.com>
2022-12-02 20:10:33 +0000

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -49,6 +49,7 @@ const sciNote = (acc) => {
 }
 const truncate = (acc) =>{
   const accLength = Math.ceil(Math.log10(acc + 1))
+  console.log(accLength)
   if (accLength <4) return acc
 for (let i = 0; i< 33; i+=3){
   if (accLength < 4 + i) return acc/(10**accLength) + (i/3 == 1 ? 'k' : i/3 == 2 ? 'm' : i/3 == 3 ? 'b' : i/3 == 4 ? 't' : '😵')
