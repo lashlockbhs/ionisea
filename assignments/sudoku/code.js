518b6d3bcf1ad5128fb3bd2d2bf756437bb736dc
ion <111394893+ionisea@users.noreply.github.com>
2022-12-03 17:32:54 +0000

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -32,7 +32,7 @@ This program starts when you click on
 
 const attempts = 1000000
 const multiplier = -1 // negatives and 0 may break this depending on mode
-const mode = trunc // 0-5, explained in the guide
+const mode = 2 // 0-5, explained in the guide
 let logs = {total: 0, jackpots: 0, highAnomalies: 0, lowAnomalies: 0, anomalies: 0, array: []} 
 
 // Notation functions
