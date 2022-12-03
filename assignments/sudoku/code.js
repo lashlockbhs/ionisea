177751cc346ed34c64d9f0848edbf21d3db696f2
ion <111394893+ionisea@users.noreply.github.com>
2022-12-03 22:17:51 +0000

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -104,7 +104,7 @@ const checkIfAnomaly = (acc)=>{
 
 registerOnclick((x,y) => {
   let acc = 0;
-  if (loglowestresult) let lowest = 1;
+  if (logLowestResult) let lowest = 1;
   for (let e=0; e < attempts; e++){
     const l = Math.random()/Math.random()
     if (lowest > l && logLowestResult) lowest = l
