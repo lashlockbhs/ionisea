43bec70ddb2348e5ea8c042d48ccfbfed5e7724a
ion <111394893+ionisea@users.noreply.github.com>
2022-12-03 23:12:15 +0000

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -34,7 +34,6 @@ This program starts when you click on the canvas,
 const attempts = 1000000
 const multiplier = 1 // negatives and 0 may break this depending on mode
 const mode = 0 // 0-6, explained in the guide
-const logLowestResult = false
 let logs = {total: 0, jackpots: 0, highAnomalies: 0, lowAnomalies: 0, anomalies: 0, avg:0, array: []} 
 
 // Notation functions
