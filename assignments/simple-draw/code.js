9654a970832275049d56eed93fe43d7f0df91039
ion <111394893+ionisea@users.noreply.github.com>
2022-12-03 23:09:57 +0000

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,4 +1,3 @@
-/*
 drawFilledRect (0, 0, width, height, 'black'); // use the red text to change the colors
 var fujkYou = -10000
 while (fujkYou <1000){
@@ -23,45 +22,3 @@ while (shithead >-190000) {
 //drawFilledRect (width/2 + 40, height/2 + 100, 60, 80, 'blue');
 //drawFilledRect (width/2 -75, height/2 - 120, 150, 60, 'teal');
 //drawFilledRect (width/2 + 100, height/2 - 100, 50, 130, 'white');
-*/
-const z_sqr = (x,y) =>{
-  return [x**2 - y**2, 2*x*y];
-}
-const f = (z, c) =>{
-  return [z_sqr(z[0], z[1])[0] + c[0], z_sqr(z[0], z[1])[1] + c[1]]
-}
-const isPixelInSet = (c, iterations) =>{
-  let z=[0,0]
-  let i=0 
-  for(i; i<iterations; i++){
-    z=f(z, c);
-    if(!isFinite(z[0])||!isFinite(z[1])){
-    return i
-    }
-  }
-  if(z[0]>2||z[1]>2){
-    return i
-  }
-  return 0
-}
-const drawmandel = (iterations, bx, by, centerx, centery, zoom, c) =>{
-  const ofx=-((centerx/(width/bx))*zoom);
-  const ofy=((centery/(height/by))*zoom);
-  let xm;
-  let ym;
-  drawLine(by/2, 0, bx/2, by, 'black')
-  drawLine(0, by/2, bx, by/2, 'black')
-  for(let y=-ofy; y<=by-ofy; y++){
-    for(let x=-ofx; x<=bx-ofx; x++){
-      xm=(-2-(ofx/bx))/zoom+((4/zoom)/bx)*x
-      ym=(2+(ofy/by))/zoom-((4/zoom)/by)*y
-      let pixelinset = isPixelInSet([xm, ym], iterations)
-      pixelinset===0 ? drawLine(x+ofx, y+ofy, x+1+ofx, y+ofy, c) : drawLine(x+ofx, y+ofy, x+1+ofx, y+ofy, 'hsl(' + (pixelinset*2) + ', 100%, 50%)')
-    }
-  }
-}
-const x = 31.0001
-const y = 0 
-const maxiterations = 10000
-const zoom = 1
-drawmandel(maxiterations, height, height, x, y, zoom, 'black')
