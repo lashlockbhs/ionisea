67d42a4224445294181476a99669d6114bef3aa7
ion <111394893+ionisea@users.noreply.github.com>
2022-12-03 23:05:11 +0000

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -60,8 +60,8 @@ const drawmandel = (iterations, bx, by, centerx, centery, zoom, c) =>{
     }
   }
 }
-const x = 35 
+const x = 30 
 const y = 0 
 const maxiterations = 10000
-const zoom = 12
+const zoom = 20
 drawmandel(maxiterations, height, height, x, y, zoom, 'black')
