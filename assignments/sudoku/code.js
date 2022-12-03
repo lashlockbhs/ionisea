c7ca788c46652ef82be4933add6d8b007b13536b
ion <111394893+ionisea@users.noreply.github.com>
2022-12-03 17:52:05 +0000

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -46,8 +46,8 @@ const commas = (num) =>{
   if (returnString[0] == ',') returnString = returnString.substring(1)
   return returnString;
 }
-const sciNote = (acc) => {
- return (acc/10**Math.ceil(Math.log10(acc/10))).toString().substring(0,5)+ " * 10 ^ " + (Math.ceil(Math.log10(acc + 1))-1)
+const sciNote = (acc, e) => {
+ return (acc/10**Math.ceil(Math.log10(acc/10))).toString().substring(0,5)+ (e ? " * 10 ^ " : 'e+') + (Math.ceil(Math.log10(acc + 1))-1)
 }
 const truncate = (acc) =>{
   const accLength = Math.ceil(Math.log10(acc))
@@ -67,7 +67,7 @@ const averageResults = (array) =>{
 const convertAccMode = (acc) =>{
   if ((mode < 1) || (mode >5)){ return acc
   } else if (mode == 1) {
-    return sciNote(acc)
+    return sciNote(acc, false)
   } else if (mode == 2){
     return Math.ceil(Math.log10(acc))
   } else if (mode == 3){
@@ -76,6 +76,8 @@ const convertAccMode = (acc) =>{
     return averageResults(logs.array)
   } else if (mode == 5){
     return commas(acc)
+  } else if (mode == 6){
+    sciNote(acc, true)
   }
 }
 //Check for anomaly
