901e7fa90ceba3f0351361b38c32e02af8e4f20f
ion <111394893+ionisea@users.noreply.github.com>
2022-12-06 22:46:55 +0000

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -65,7 +65,7 @@ const averageResults = (array) =>{
 // Mode check
 const convertAccMode = (acc, l) =>{
   if (mode == 0){
-    return l.toPrecision(Math.ceil(Math.log10(attempts+1)))
+    return l.toPrecision(Math.ceil(Math.log10(attempts+1))-3)
   } else if (mode == 1) {
     return sciNote(acc)
   } else if (mode == 2){
