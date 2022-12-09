f2cd7a74e072239681a7c6cb9729c8d99edf5b95
ion <111394893+ionisea@users.noreply.github.com>
2022-12-09 22:36:21 +0000

    Updating demo/repl/code.js

--- demo/repl/code.js
+++ demo/repl/code.js
@@ -1,3 +1,4 @@
+/*
 // Warning! Do not call this function with numbers much bigger than 40 unless
 // you want to kill this tab.
 const fib = (n) => (n < 2 ? n : fib(n - 2) + fib(n - 1));
@@ -16,3 +17,26 @@ const fib2 = (n) => {
 const MAX_FIB_N = 1476;
 
 const MAX_FIB = fib2(MAX_FIB_N);
+*/
+const vector = (angle, magnitude) => {
+  return ({ angle: angle * Math.PI / 180, magnitude })
+}
+
+const add2Vectors = (a) => {
+  let x1 = Math.cos(a[0].angle) * a[0].magnitude
+  let x2 = Math.cos(a[1].angle) * a[1].magnitude
+  let y1 = Math.sin(a[0].angle) * a[0].magnitude
+  let y2 = Math.sin(a[1].angle) * a[1].magnitude
+  let angle = Math.atan2(y1 + y2, x1 + x2)
+  let mag = Math.sqrt((x1 + x2) ** 2 + (y1 + y2) ** 2)
+  return ({ angle: angle * 180 / Math.PI, magnitude: mag })
+}
+
+const addNumVectors = (a) => {
+  let accVector = vector(0,0)
+  for (const element of a){
+    accVector = add2Vectors(accVector, element)
+  }
+  return accVector
+  //return a.reduce((acc, x) => add2Vectors([acc, x]), vector(0, 0))
+}
