5a9e4a40093cdb4d29a5025a452c003505bc8c59
ion <111394893+ionisea@users.noreply.github.com>
2023-02-15 02:57:06 +0000

    interesting little thing

--- projects/autoDraw/public/script.js
+++ projects/autoDraw/public/script.js
@@ -1,51 +1,18 @@
-import { setCanvas, drawFilledCircle, clear, width, height, animate, now } from './graphics.js';
-
-const drawBall = (b) => {
-  drawFilledCircle(b.x, b.y, b.size, 'blue');
-};
-
-let lastFrame = now();
-
-const updatePosition = (b, t) => {
-  maybeBounce(b);
-  b.x = clamp(b.x + b.dx * (t - lastFrame), 0, width);
-  b.y = clamp(b.y + b.dy * (t - lastFrame), 0, height);
-  lastFrame = t;
-};
-
-const distance = (a, b) => Math.abs(a - b);
-
-const clamp = (n, min, max) => (n < min ? min : n > max ? max : n);
-
-const maybeBounce = (b) => {
-  if (Math.min(distance(b.x, 0), distance(b.x, width)) < b.size) {
-    b.dx *= -1;
-  }
-  if (Math.min(distance(b.y, 0), distance(b.y, height)) < b.size) {
-    b.dy *= -1;
-  }
-};
-
-const launch = (b, speed, direction) => {
-  b.dx = Math.cos(direction) * speed;
-  b.dy = Math.sin(direction) * speed;
-};
-
-// This has to come early so width and height are set before we use them.
+import { setCanvas, drawFilledCircle, clear, width, height, animate, now, drawLine, drawFilledRect } from './graphics.js';
 setCanvas(document.getElementById('screen'));
-
-let ball = {
-  x: width / 2,
-  y: height / 2,
-  size: 10,
-  dx: 0,
-  dy: 0,
-};
-
-launch(ball, 1, Math.random() * Math.PI * 2);
-
+drawFilledRect(0, 0, width, height, ' #002082')
+const length = 10
+const RoM = 4/3 * Math.PI
+let angle = 0
+let coords = { x: width / 2, y: height / 2 }
 animate((t) => {
-  updatePosition(ball, t);
-  clear();
-  drawBall(ball);
+  //random
+  //angle += (Math.random()-0.5) * RoM/2
+  //edges of range of motion only
+  angle += Math.round(Math.random() * 2 - 1) * RoM/2
+  const b = Math.sin(angle) * length
+  const p = Math.cos(angle) * length
+  const newCoords = { x: coords.x + b, y: coords.y + p }
+  drawLine(coords.x, coords.y, newCoords.x, newCoords.y, 'white')
+  coords = newCoords
 });
