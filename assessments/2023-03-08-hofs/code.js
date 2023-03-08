e92708e36152b000c6e39f0dd2be6c43ffef0bd3
ion <111394893+ionisea@users.noreply.github.com>
2023-03-08 14:36:01 -0800

    Updating assessments/2023-03-08-hofs/code.js

--- assessments/2023-03-08-hofs/code.js
+++ assessments/2023-03-08-hofs/code.js
@@ -1,9 +1,17 @@
 const evens = (numbers) => {
   const noRm = []
-  for (const element of numbers){
-    if (element %2 === 0){
+  for (const element of numbers) {
+    if (element % 2 === 0) {
       noRm.push(element)
     }
   }
   return noRm
+};
+
+const shouty = (strings) => {
+  const loud = []
+  for (const element of strings) {
+    loud.push(element.toUpperCase())
+  }
+  return loud
 };
