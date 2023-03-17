9da69630d152850632289bf3cdb4ebcb0033f470
ionisea <ioncook@students.berkeley.net>
2023-03-17 16:04:46 +0000

    Can you not make live changes to body?

--- projects/simple-capital-game/public/scripts/script.js
+++ projects/simple-capital-game/public/scripts/script.js
@@ -1,18 +1,27 @@
-import countries from countries.js
+import {countries} from './countries.js'
 
 const body = document.querySelector('body')
-const correctDisplay = body.append()
+const statDisplay = document.createElement('h3')
+const countryDisplay = document.createElement('h1')
 let currentCountry = countries[Math.floor(Math.random() * countries.length)]
-let stats = {correct: 0, wrong: 0}
+let stats = { correct: 0, wrong: 0 }
 
-const nextQ = () =>{
+window.onload = () => body.append(statDisplay, countryDisplay)
+
+const nextQ = () => {
   body.textContent = '';
+  statDisplay.append('correct: ' + stats.correct + '; incorrect: ' + stats.wrong);
+  countryDisplay.append('What is the capital of ' + currentCountry.country + '?');
+  currentCountry = countries[Math.floor(Math.random() * countries.length)];
+};
 
-}
-
-document.onkeydown = ((e)=>{
-  if (e.key == "Enter"){
-    //check for correctness
+document.onkeydown = ((e) => {
+  if (e.key == "Enter") {
+    if (document.getElementById('answer').value === currentCountry.capital) {
+      stats.correct++
+    } else {
+      stats.wrong++
+    };
     nextQ();
-  }
-})
+  };
+});
