98afb620c163594f0692002052b8a4d50734a62d
ionisea <ioncook@students.berkeley.net>
2023-03-18 08:14:38 +0000

    temporary solution

--- projects/simple-capital-game/public/scripts/script.js
+++ projects/simple-capital-game/public/scripts/script.js
@@ -1,27 +1,47 @@
 import {countries} from './countries.js'
+import graphics from './graphics.js'
 
-const body = document.querySelector('body')
-const statDisplay = document.createElement('h3')
-const countryDisplay = document.createElement('h1')
-let currentCountry = countries[Math.floor(Math.random() * countries.length)]
+/*
+const body = document.getElementById('body')
+let statDisplay = document.createElement('h3')
+let countryDisplay = document.createElement('h1')
+*/
+
+const canvas = document.getElementById('screen')
+let currentCountry //= countries[Math.floor(Math.random() * countries.length)]
 let stats = { correct: 0, wrong: 0 }
+const g = graphics(canvas)
+const width = canvas.width
+const height = canvas.height
 
-window.onload = () => body.append(statDisplay, countryDisplay)
+
+window.onload = () => {
+  /*countryDisplay.append(document.createTextNode('What is the capital of ' + currentCountry.country + '?'));
+  statDisplay.append(document.createTextNode('correct: ' + stats.correct + '; incorrect: ' + stats.wrong));
+  body.append(statDisplay, countryDisplay)*/
+  nextQ();
+}
 
 const nextQ = () => {
-  body.textContent = '';
+  /*statDisplay.textContent = '';
+  countryDisplay.textContent = '';
   statDisplay.append('correct: ' + stats.correct + '; incorrect: ' + stats.wrong);
-  countryDisplay.append('What is the capital of ' + currentCountry.country + '?');
+  countryDisplay.append('What is the capital of ' + currentCountry.country + '?');*/
+  g.clear();
   currentCountry = countries[Math.floor(Math.random() * countries.length)];
+  g.drawText('correct: ' + stats.correct + '; incorrect: ' + stats.wrong, width/4, height/2 - 15, 'black', 18)
+  g.drawText('What is the capital of ' + currentCountry.country + '?', 0, height/2+15, 'black', 22)
+  
 };
 
 document.onkeydown = ((e) => {
-  if (e.key == "Enter") {
+  if ((e.key == "Enter") && (document.getElementById('answer').value != '')) {
     if (document.getElementById('answer').value === currentCountry.capital) {
       stats.correct++
     } else {
       stats.wrong++
     };
+    document.getElementById('answer').value = ''
     nextQ();
   };
 });
