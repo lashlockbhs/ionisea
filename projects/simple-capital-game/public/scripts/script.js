b8c31713368dc1264b297349b5a14469f9642d40
ionisea <ioncook@students.berkeley.net>
2023-03-18 09:56:58 +0000

    fixed some syntactic errors and various other things

--- projects/simple-capital-game/public/scripts/script.js
+++ projects/simple-capital-game/public/scripts/script.js
@@ -24,10 +24,11 @@ window.onload = () => {
   nextQ();
 }
 
-const fillAnswer = () => {
+const fillAnswer = (uncapped) => {
   g2.clear();
   const string = `The capital of ${currentCountry.country} is ${currentCountry.capital}.`
   g2.drawText(string, (g2.width - string.length * 7.5) / 2, g2.height / 2 + 7.5, 'black', 15)
+  if (uncapped) g2.drawText('Use correct capitalization!', (g2.width - 125) / 2, g2.height / 2 + 25, 'black', 10)
 }
 const nextQ = () => {
   /*statDisplay.textContent = '';
@@ -37,19 +38,19 @@ const nextQ = () => {
   g1.clear();
   currentCountry = countries[Math.floor(Math.random() * countries.length)];
   g1.drawText('correct: ' + stats.correct + '; incorrect: ' + stats.wrong, (g1.width - getStatDisplayLength() * 9) / 2, g1.height / 2 - 15, 'black', 18)
-  g1.drawText('What is the capital of ' + currentCountry.country + '?', (g1.width - getCountryDisplayLength() * 11) / 2, g1.height / 2 + 15, 'black', 22)
-
+  g1.drawText(`What is the capital of ${currentCountry.country}?`, (g1.width - getCountryDisplayLength() * 11) / 2, g1.height / 2 + 15, 'black', 22)
 };
 
 document.onkeydown = ((e) => {
-  if ((e.key == "Enter") && (document.getElementById('answer').value != '')) {
-    if (document.getElementById('answer').value === currentCountry.capital) {
+  const input = document.getElementById('answer').value
+  if ((e.key == "Enter") && (input != '')) {
+    if (input === currentCountry.capital) {
       stats.correct++
     } else {
       stats.wrong++
     };
     document.getElementById('answer').value = ''
-    fillAnswer();
+    fillAnswer(input[0] === input[0].toLowerCase());
     nextQ();
   };
 });
