53c8d76fc972d7f66fb8faf07f74c49e837bb699
ionisea <ioncook@students.berkeley.net>
2023-03-19 19:06:08 +0000

    Better?

--- projects/simple-capital-game/public/scripts/script.js
+++ projects/simple-capital-game/public/scripts/script.js
@@ -1,21 +1,28 @@
 import { countries } from './countries.js'
 import graphics from './graphics.js'
 
-/*
-const body = document.getElementById('body')
-let statDisplay = document.createElement('h3')
-let countryDisplay = document.createElement('h1')
-*/
 
-const canvasA = document.getElementById('screen')
-const canvasB = document.getElementById('answers')
-let currentCountry //= countries[Math.floor(Math.random() * countries.length)]
+const body = document.body
+let statDisplay = document.getElementById('stats')
+let countryDisplay = document.getElementById('question')
+let correctDisplay = document.getElementById('answers')
+let pastAnswers = []
+
+//const canvasA = document.getElementById('screen')
+//const canvasB = document.getElementById('answers')
+let currentCountry = countries[Math.floor(Math.random() * countries.length)]
 let stats = { correct: 0, wrong: 0 }
-const g1 = graphics(canvasA)
-const g2 = graphics(canvasB)
+//const g1 = graphics(canvasA)
+//const g2 = graphics(canvasB)
 
-const getStatDisplayLength = () => ('correct: ' + stats.correct + '; incorrect: ' + stats.wrong).length
-const getCountryDisplayLength = () => ('What is the capital of ' + currentCountry.country + '?').length
+const createLi = (text) => {
+  const li = document.createElement('li')
+  li.append(text)
+  return li
+}
+
+//const getStatDisplayLength = () => ('correct: ' + stats.correct + '; incorrect: ' + stats.wrong).length
+//const getCountryDisplayLength = () => ('What is the capital of ' + currentCountry.country + '?').length
 
 window.onload = () => {
   /*countryDisplay.append(document.createTextNode('What is the capital of ' + currentCountry.country + '?'));
@@ -24,33 +31,34 @@ window.onload = () => {
   nextQ();
 }
 
-const fillAnswer = (wrongcaps) => {
-  g2.clear();
-  const string = `The capital of ${currentCountry.country} is ${currentCountry.capital}.`
-  g2.drawText(string, (g2.width - string.length * 7.5) / 2, g2.height / 2 + 7.5, 'black', 15)
-  if (wrongcaps) g2.drawText('Use correct capitalization!', (g2.width - 125) / 2, g2.height / 2 + 25, 'black', 10)
+const fillAnswer = (wrongcaps, correct, answer) => {
+  let string = `The capital of ${currentCountry.country} is ${currentCountry.capital}. ` + `(your answer: ${answer} ${correct ? '✓' : '✗'})`
+  pastAnswers = [string].concat(pastAnswers)
+  correctDisplay.textContent = ''
+  pastAnswers.forEach(e => { correctDisplay.append(createLi(e)) })
 }
 const nextQ = () => {
-  /*statDisplay.textContent = '';
+  statDisplay.textContent = '';
   countryDisplay.textContent = '';
-  statDisplay.append('correct: ' + stats.correct + '; incorrect: ' + stats.wrong);
-  countryDisplay.append('What is the capital of ' + currentCountry.country + '?');*/
-  g1.clear();
   currentCountry = countries[Math.floor(Math.random() * countries.length)];
-  g1.drawText('correct: ' + stats.correct + '; incorrect: ' + stats.wrong, (g1.width - getStatDisplayLength() * 9) / 2, g1.height / 2 - 15, 'black', 18)
-  g1.drawText(`What is the capital of ${currentCountry.country}?`, (g1.width - getCountryDisplayLength() * 11) / 2, g1.height / 2 + 15, 'black', 22)
+  statDisplay.append('correct: ' + stats.correct + '; incorrect: ' + stats.wrong);
+  countryDisplay.append('What is the capital of ' + currentCountry.country + '?');
+  //g1.clear();
+  //g1.drawText('correct: ' + stats.correct + '; incorrect: ' + stats.wrong, (g1.width - getStatDisplayLength() * 9) / 2, g1.height / 2 - 15, 'black', 18)
+  // g1.drawText(`What is the capital of ${currentCountry.country}?`, (g1.width - getCountryDisplayLength() * 11) / 2, g1.height / 2 + 15, 'black', 22)
 };
 
 document.onkeydown = ((e) => {
-  const input = document.getElementById('answer').value
+  const input = document.getElementById('answerInput').value;
+  const maybeCorrect = input === currentCountry.capital;
   if ((e.key == "Enter") && (input != '')) {
-    if (input === currentCountry.capital) {
+    if (maybeCorrect) {
       stats.correct++
     } else {
       stats.wrong++
     };
-    document.getElementById('answer').value = ''
-    fillAnswer((input === currentCountry.capital.toLowerCase()) || (input === currentCountry.capital.toUpperCase()));
+    document.getElementById('answerInput').value = ''
+    fillAnswer((!maybeCorrect && (input.toLowerCase() === currentCountry.capital.toLowerCase())), maybeCorrect, input);
     nextQ();
   };
 });
