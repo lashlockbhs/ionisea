2cc191402dec64c47e0f6382bb209dd320feab0e
ionisea <ioncook@students.berkeley.net>
2023-03-19 19:08:10 +0000

    removed canvas things and redundancies

--- projects/simple-capital-game/public/scripts/script.js
+++ projects/simple-capital-game/public/scripts/script.js
@@ -2,18 +2,13 @@ import { countries } from './countries.js'
 import graphics from './graphics.js'
 
 
-const body = document.body
 let statDisplay = document.getElementById('stats')
 let countryDisplay = document.getElementById('question')
 let correctDisplay = document.getElementById('answers')
 let pastAnswers = []
 
-//const canvasA = document.getElementById('screen')
-//const canvasB = document.getElementById('answers')
 let currentCountry = countries[Math.floor(Math.random() * countries.length)]
 let stats = { correct: 0, wrong: 0 }
-//const g1 = graphics(canvasA)
-//const g2 = graphics(canvasB)
 
 const createLi = (text) => {
   const li = document.createElement('li')
@@ -21,13 +16,8 @@ const createLi = (text) => {
   return li
 }
 
-//const getStatDisplayLength = () => ('correct: ' + stats.correct + '; incorrect: ' + stats.wrong).length
-//const getCountryDisplayLength = () => ('What is the capital of ' + currentCountry.country + '?').length
 
 window.onload = () => {
-  /*countryDisplay.append(document.createTextNode('What is the capital of ' + currentCountry.country + '?'));
-  statDisplay.append(document.createTextNode('correct: ' + stats.correct + '; incorrect: ' + stats.wrong));
-  body.append(statDisplay, countryDisplay)*/
   nextQ();
 }
 
@@ -43,9 +33,6 @@ const nextQ = () => {
   currentCountry = countries[Math.floor(Math.random() * countries.length)];
   statDisplay.append('correct: ' + stats.correct + '; incorrect: ' + stats.wrong);
   countryDisplay.append('What is the capital of ' + currentCountry.country + '?');
-  //g1.clear();
-  //g1.drawText('correct: ' + stats.correct + '; incorrect: ' + stats.wrong, (g1.width - getStatDisplayLength() * 9) / 2, g1.height / 2 - 15, 'black', 18)
-  // g1.drawText(`What is the capital of ${currentCountry.country}?`, (g1.width - getCountryDisplayLength() * 11) / 2, g1.height / 2 + 15, 'black', 22)
 };
 
 document.onkeydown = ((e) => {
