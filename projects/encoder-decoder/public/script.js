01869b9794184aa85de5fd0418c4ac59de6aa0d1
ion <111394893+ionisea@users.noreply.github.com>
2023-03-30 22:33:07 +0000

    start

--- projects/encoder-decoder/public/script.js
+++ projects/encoder-decoder/public/script.js
@@ -1,1 +1,35 @@
 // Write your code here.
+const encodeButton = document.getElementById('encode')
+const decodeButton = document.getElementById('decode')
+const textInput = document.getElementById('textInput')
+const keyInput = document.getElementById('keyInput')
+
+const possibleChars = '`1234567890-=~!@#$%^&*()_+qwertyuiop[]QWERTYUIOP{}|asdfghjkl;ASDFGHJKL:"zxcvbnm,./ZXCVBNM<>? '
+// missing ' and \ hel[]
+
+const generateKey = () =>{
+    const characters = '0123456789abcdefghijklmnopqrstuvwxyz'
+    let s = ''
+    for(let i = 0; i <16; i++){
+        s+=characters[Math.floor(Math.random()*characters.length)]
+    }
+    return s;
+    //return Math.round((Math.random()+1)*2**100).toString(36)
+}
+
+const encode = (text, key) =>{
+    let newText = '';
+    for(let c = 0; c<text.length -1; c++){
+        
+    }
+    return newText;
+}
+
+encodeButton.onclick = (e) =>{
+    if (keyInput.value == '') keyInput.value = generateKey()
+    encode(decodeButton.value)
+}
+
+decodeButton.onclick = (e) =>{
+   if (keyInput.value == '') alert(`Provide a key!`)
+}
