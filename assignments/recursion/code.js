547f764525f09e77debf03ece05f6446e24ea0ee
ion <111394893+ionisea@users.noreply.github.com>
2023-04-10 15:27:34 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -8,9 +8,9 @@ const triangular = (n) =>{
 
 const fibonacci = (n) => {
   if (n === 1){
-    return 0
-  } if (n === 0){
     return 1
+  }else  if (n === 0){
+    return 0
   } else {
     return fibonacci(n-2) + fibonacci(n-1);
   }
