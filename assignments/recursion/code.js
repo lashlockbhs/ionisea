0342f6ed566465883c10539582a7efd99d039c16
ion <111394893+ionisea@users.noreply.github.com>
2023-04-14 14:53:12 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -67,6 +67,14 @@ const treeMap = (branch, op) => {
 
 
 const change = (am, coins) => {
+  if (am === 0) {
+    return 1;
+  } else if (coins === []){
+    return 0;
+  } else {
+    return change(am - coins [0], coins) + change(am, coins.slice(1))
+  }
+
  /* if (am === 0) {
     return 1
   } else if (am >= Math.min(...coins)) {
