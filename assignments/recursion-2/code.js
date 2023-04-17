9cf646b347f352cae173af530c86a074f5170c8a
ion <111394893+ionisea@users.noreply.github.com>
2023-04-17 15:06:45 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -1,33 +1,41 @@
 const add = (a, b) => {
   if (b === 0) {
-    return a
+    return a;
   } else {
-    return add(a + 1, b - 1)
-  }
-}
+    return add(a + 1, b - 1);
+  };
+};
 
-const multiply = (a, b) => {
+const multiply = (a, b) => { //this one looks improvable
   if (a === 0 || b === 0) {
-    return 0
+    return 0;
   } else if (b === 1) {
-    return a
+    return a;
   } else {
-    return multiply(a * b, b / b)
-  }
-}
+    return multiply(a * b, b / b);
+  };
+};
 
 const double = (n, a) => {
   if (a === 0){
-    return n
+    return n;
   } else {
-    return double(n*2, a-1)
-  }
-}
+    return double(n*2, a-1);
+  };
+};
 
 const triple = (n, a) => {
   if (a === 0){
-    return n
+    return n;
   } else {
-    return triple(n*3, a-1)
+    return triple(n*3, a-1);
+  };
+};
+
+const power = (n, a) =>{
+  if (a === 0){
+    return n;
+  } else {
+    return power(n*n, a-1)
   }
 }
