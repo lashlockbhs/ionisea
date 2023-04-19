f95c9b145edbe9739ccbf948976b76f1b14c2441
ion <111394893+ionisea@users.noreply.github.com>
2023-04-19 15:10:50 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -70,7 +70,7 @@ const every = (arr, p) => {
   if (arr.length === 0) {
     return true
   } else {
-    return !p(arr[0]) && every(arr.slice(1), p)
+    return every(arr.slice(1), p) && !p(arr[0])
   }
 }
 
