ea535aebcf7da9bb13d5cfb50c658a4ce85b8567
ion <111394893+ionisea@users.noreply.github.com>
2023-04-21 14:49:16 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -1,18 +1,22 @@
 ////////////////////////////////////////////////////////////////
 // Write your code here ...
 
-const factorial = (n) => n === 0 ? 1 : n*factorial(n-1)
+const factorial = (n) => n === 0 ? 1 : n * factorial(n - 1)
 
-const fibonacci = (n) => n < 2 ? n : fibonacci(n-1) + fibonacci (n-2)
+const fibonacci = (n) => n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2)
 
-const sumSquares = (n) => n === 0 ? 0 : n**2 + sumSquares(n-1)
+const sumSquares = (n) => n === 0 ? 0 : n ** 2 + sumSquares(n - 1)
 
 const maximum = (arr) => arr.length === 0 ? -Infinity : Math.max(arr[0], maximum(arr.slice(1)))
 
-const treeMap = (branch, op) => {
-  if (isLeaf(branch)){
-    return op(branch)
-  } else return {left: treeMap(branch.left, op), right: treeMap(branch.right, op)}
+const treeMap = (branch, op) => isLeaf(branch) ? op(branch) :
+  { left: treeMap(branch.left, op), right: treeMap(branch.right, op) };
+
+const sumPrimesBelow = n => {
+  if (n === 0) return n
+  else if (isPrime(n)) {
+    return n+sumPrimesBelow(n-1)
+  } else return sumPrimesBelow(n-1)
 }
 
 ////////////////////////////////////////////////////////////////
