40ef41b1e205badba51e73b6c4c51fb951a001ae
ion <111394893+ionisea@users.noreply.github.com>
2023-04-21 14:54:51 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -20,7 +20,7 @@ const sumPrimesBelow = n => n === 0 ? n :
 const nvwls = (str) => {
   if (str.length === 0) {
     return
-  } else if (vowels.indexOf(str[0]) === -1) {
+  } else if (vowels.indexOf(str[0]) !== -1) {
     return nvwls(str.slice(1))
   } else return str[0] + nvwls(str.slice(1))
 }
