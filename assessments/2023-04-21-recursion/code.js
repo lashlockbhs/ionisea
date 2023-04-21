f8f5c1b0f678091a158d669879d2e3a7904dd6ed
ion <111394893+ionisea@users.noreply.github.com>
2023-04-21 14:45:22 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -7,12 +7,12 @@ const fibonacci = (n) => n < 2 ? n : fibonacci(n-1) + fibonacci (n-2)
 
 const sumSquares = (n) => n === 0 ? 0 : n**2 + sumSquares(n-1)
 
-const maximum = (arr) => {
-  if (arr.length === 0) {
-    return -Infinity
-  } else {
-    return Math.max(arr[0], maximum(arr.slice(1)))
-  }
+const maximum = (arr) => arr.length === 0 ? -Infinity : Math.max(arr[0], maximum(arr.slice(1)))
+
+const treeMap = (branch, op) => {
+  if (isLeaf(branch)){
+    return op(branch)
+  } else return {left: treeMap(branch.left, op), right: treeMap(branch.right, op)}
 }
 
 ////////////////////////////////////////////////////////////////
