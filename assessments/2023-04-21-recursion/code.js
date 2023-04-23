be692ff6cf3566d9dbb345b448857c9848934b5c
Peter Seibel <peter@gigamonkeys.com>
2023-04-23 14:20:40 -0700

    Grading 2023-04-21-recursion

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -1,23 +1,24 @@
 /* ---------------------------------------------------------------- */
 // Write your code here ...
 
-const factorial = (n) => n === 0 ? 1 : n * factorial(n - 1)
+const factorial = (n) => n === 0 ? 1 : n * factorial(n - 1) // correct
 
-const fibonacci = (n) => n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2)
+const fibonacci = (n) => n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2) // correct
 
-const sumSquares = (n) => n === 0 ? 0 : n ** 2 + sumSquares(n - 1)
+const sumSquares = (n) => n === 0 ? 0 : n ** 2 + sumSquares(n - 1) // correct
 
-const maximum = (arr) => arr.length === 0 ? -Infinity : Math.max(arr[0], maximum(arr.slice(1)))
+const maximum = (arr) => arr.length === 0 ? -Infinity : Math.max(arr[0], maximum(arr.slice(1))) // correct
 
-const treeMap = (branch, op) => isLeaf(branch) ? op(branch) :
+const treeMap = (branch, op) => isLeaf(branch) ? op(branch) : // correct
   { left: treeMap(branch.left, op), right: treeMap(branch.right, op) };
 
-const sumPrimesBelow = n => n === 0 ? n :
+const sumPrimesBelow = n => n === 0 ? n : // correct
   isPrime(n) ? n + sumPrimesBelow(n - 1) :
     sumPrimesBelow(n - 1)
   ;
 
-const nvwls = (str) => { // i defined a helper var, hope that's ok
+// i defined a helper var, hope that's ok
+const nvwls = (str) => { // correct
   if (str.length === 0) {
     return ''
   } else if (vowels.indexOf(str[0]) !== -1) { // this could be done with a bunch of booleans but its a bit of a hassle
@@ -25,11 +26,11 @@ const nvwls = (str) => { // i defined a helper var, hope that's ok
   } else return str[0] + nvwls(str.slice(1))
 }
 
-const caesar = (str, key) => str.length === 0 ? '' : rotate(str[0], key) + caesar(str.substring(1), key);
+const caesar = (str, key) => str.length === 0 ? '' : rotate(str[0], key) + caesar(str.substring(1), key); // correct
 
-const toList = (arr) => arr.length === 0 ? null : { first: arr[0], rest: toList(arr.slice(1)) };
+const toList = (arr) => arr.length === 0 ? null : { first: arr[0], rest: toList(arr.slice(1)) }; // correct
 
-const map = (branch, op) => branch.rest === null ?
+const map = (branch, op) => branch.rest === null ? // almost
   { first: op(branch.first), rest: null }
   : { first: op(branch.first), rest: map(branch.rest, op) }
   ;
@@ -37,7 +38,8 @@ const map = (branch, op) => branch.rest === null ?
 /* ---------------------------------------------------------------- */
 //challenge problems
 
-const div10Not100 = (i) => { // a bit scuffed but works until 12451
+// a bit scuffed but works until 12451
+const div10Not100 = (i) => {
   if (i === 0) return 0
   else {
     if ((i / 100) === Math.round(i / 100) || ((i / 10) !== Math.round(i / 10))) {
@@ -48,7 +50,8 @@ const div10Not100 = (i) => { // a bit scuffed but works until 12451
   }
 }
 
-const whichDiv10Not100 = (n) => { //significantly easier without worrying about recursion
+//significantly easier without worrying about recursion
+const whichDiv10Not100 = (n) => {
   let x = 0;
   while (div10Not100(x) < n) {
     x += 10;
